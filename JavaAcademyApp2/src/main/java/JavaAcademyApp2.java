19222fa2e842a565e5c65d0545f2d7c48070a7c3
Vadim <vadim.sudarenko1998@gmail.com>
2022-06-14 16:23:30 +0300

    last changes

--- JavaAcademyApp2/src/main/java/JavaAcademyApp2.java
+++ JavaAcademyApp2/src/main/java/JavaAcademyApp2.java
@@ -3,17 +3,17 @@ import java.util.Map;
 
 public class JavaAcademyApp2 {
     public static void characterCount(String inputString) {
-        HashMap<Character, Integer> charCountMap = new HashMap< Character, Integer >();
+        Map<Character, Integer> charCountMap = new HashMap<Character, Integer>();
         char[] strArray = inputString.toCharArray();
         for (char c : strArray) {
-            if (charCountMap.containsKey(c)) {
-
-                charCountMap.put(c, charCountMap.get(c) + 1);
-            } else {
-                charCountMap.put(c, 1);
+                if (charCountMap.containsKey(c)) {
+                    charCountMap.put(c, charCountMap.get(c) + 1);
+                } else {
+                    charCountMap.put(c, 1);
+                }
             }
-        }
-        for ( Map.Entry entry : charCountMap.entrySet()){
+
+        for (Map.Entry entry : charCountMap.entrySet()){
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
